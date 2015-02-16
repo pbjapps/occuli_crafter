c9204e73ef325fedcc03a5e42ca3c97ab7b6caf3
Janoo Fernandes <smoothclip@gmail.com>
2015-02-15 19:46:20 -0500

    Fix broken value

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'oculi-crafter',
     environment: environment,
     baseURL: '/oculi-crafter',
-    locationType: 'hash,
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
