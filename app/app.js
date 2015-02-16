ede21662504a40a67e23c53b9c0ce94769f7617f
Janoo Fernandes <smoothclip@gmail.com>
2015-02-15 20:22:51 -0500

    Change App to OculiCrafter

--- app/app.js
+++ app/app.js
@@ -5,12 +5,12 @@ import config from './config/environment';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-var App = Ember.Application.extend({
+var OculiCrafter = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
 });
 
-loadInitializers(App, config.modulePrefix);
+loadInitializers(OculiCrafter, config.modulePrefix);
 
-export default App;
+export default OculiCrafter;
