c031f4d6fde91aaa028577212730b6b9c696bd5c
dddotcom <ddortega@bu.edu>
2015-04-19 14:15:50 -0400

    adding Crafter model with slot attributes

---
+++ app/models/crafter.js
@@ -0,0 +1,22 @@
+import DS from 'ember-data';
+
+var Crafter = DS.Model.extend({
+  slot1: DS.belongsTo('oculi'),
+  slot2: DS.belongsTo('oculi'),
+  slot3: DS.belongsTo('oculi'),
+
+  // isValidFormula: function(){
+  //   if () {
+  //     return true
+  //   } else {
+  //     return false
+  //   }
+  // }.property('slot1', 'slot2', 'slot3')
+  //
+  // craftOculi: function(){
+  //   return Oculi
+  // }.property('slot1', 'slot2', 'slot3')
+
+});
+
+export default Crafter;
